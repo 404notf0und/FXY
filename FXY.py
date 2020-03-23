db30f0d0599709a57569ae4bd09c7f1120088953
404notfound <2811832487@qq.com>
2020-03-23 16:23:48 +0800

    [+] test

--- FXY.py
+++ FXY.py
@@ -1,19 +1,15 @@
 from feature_vec.data import data_load
 from feature_vec.nlp2vec import tfidf,wordindex,word2vec
-from feature_vec.model import _LSTM,_CNN
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from feature_vec.model import lstm_3D,lstm_2D
 from sklearn.metrics import classification_report
 
-x1,y1,x2,y2=data_load('part1.csv','part3.csv')
-nlp=word2vec(one_class=True,vocabulary_size=300,out_dimension=2)
+x1,y1,x2,y2=data_load('part1.csv','part2.csv')
+nlp=wordindex()
 fx1,fy1=nlp.fit_transform(x1,y1)
 fx2=nlp.transform(x2)
-#print(nlp.dictionary_count)
-lr=LogisticRegression()
-lr.fit(fx1,fy1)
-r2=lr.predict(fx2)
+train_x, valid_x, train_y, valid_y = train_test_split( fx1, fy1, random_state=2019,test_size = 0.2) 
+model=lstm_2D(nlp.max_length,nlp.input_dim,8)
+model.fit(train_x, train_y, validation_data=(valid_x,valid_y), epochs=1, batch_size=128)
+r2=model.predict_classes(fx2)
 print(classification_report(y2,r2))
-#train_x, valid_x, train_y, valid_y = train_test_split( fx1, fy1, random_state=2019,test_size = 0.2) 
-#model=_CNN(wd.max_log_length,wd.input_dim,wd.output_dim)
-#model.fit(train_x, train_y, validation_data=(valid_x,valid_y), epochs=1, batch_size=128)
--- README.md
+++ README.md
@@ -23,10 +23,20 @@ FXY is an open-sourced feature engineering framework for data preprocessing, ana
     pip install -r requirement.txt
 
 # Usage
+	from feature_vec.data import data_load
 	from feature_vec.nlp2vec import tfidf,wordindex,word2vec
+	from sklearn.linear_model import LogisticRegression
+	from sklearn.metrics import classification_report
+
+	x1,y1,x2,y2=data_load('part1.csv','part3.csv')
 	nlp=word2vec(one_class=True,vocabulary_size=300,out_dimension=2)
 	fx1,fy1=nlp.fit_transform(x1,y1)
 	fx2=nlp.transform(x2)
+
+	lr=LogisticRegression()
+	lr.fit(fx1,fy1)
+	r2=lr.predict(fx2)
+	print(classification_report(y2,r2))
 
 # Documentation
 - Demand and Design
