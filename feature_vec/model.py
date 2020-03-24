b74c5b0fbe40a38a5f447fe5b9cbc203f1e9379a
404notfound <2811832487@qq.com>
2020-03-24 17:53:07 +0800

    [+] add textcnn_2D

--- feature_vec/model.py
+++ feature_vec/model.py
@@ -11,7 +11,7 @@ def lstm_2D(max_len,input_dim,output_dim):
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
-    
+
 def lstm_3D(max_len,input_dim):
     model = Sequential()
     model.add(InputLayer(input_shape=(max_len,input_dim)))
@@ -22,26 +22,24 @@ def lstm_3D(max_len,input_dim):
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-# def _CNN(max_log_length,input_dim,output_dim):
-#     # word level
-#     kernel_size = [2,3,4,5]
-#     acti = 'relu'
-#     my_input = Input(shape=(max_log_length,), dtype='int32')
-#     emb = Embedding(input_dim, output_dim, input_length=max_log_length)(my_input)
-#     emb = SpatialDropout1D(0.2)(emb)
+def textcnn_2D(max_len,input_dim,output_dim):
+    kernel_size = [2,3,4,5]
+    my_input = Input(shape=(max_len,), dtype='float64')
+    emb = Embedding(input_dim, output_dim, input_length=max_len)(my_input)
+    emb = SpatialDropout1D(0.2)(emb)
 
-#     net = []
-#     for kernel in kernel_size:
-#         con = Conv1D(32, kernel, activation=acti, padding="same")(emb)
-#         con = MaxPool1D(2)(con)
-#         net.append(con)
-#     net = concatenate(net, axis=-1)
-#     net = Flatten()(net)
-#     net = Dropout(0.5)(net)
-#     net = Dense(64, activation='relu')(net)
-#     net = Dropout(0.5)(net)
-#     net = Dense(1, activation='softmax')(net)
-#     model = Model(inputs=my_input, outputs=net)
-#     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-#     return model
+    net = []
+    for kernel in kernel_size:
+        con = Conv1D(32, kernel, activation='relu', padding="same")(emb)
+        con = MaxPool1D(2)(con)
+        net.append(con)
+    net = concatenate(net, axis=-1)
+    net = Flatten()(net)
+    net = Dropout(0.5)(net)
+    net = Dense(64, activation='relu')(net)
+    net = Dropout(0.5)(net)
+    net = Dense(1, activation='sigmoid')(net)
+    model = Model(inputs=my_input, outputs=net)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
