62431059a8f12a90e215d798aedf90473caaeb8b
404notfound <2811832487@qq.com>
2020-03-28 15:55:33 +0800

    [+] add textcnn_3D

--- feature_vec/model.py
+++ feature_vec/model.py
@@ -24,7 +24,7 @@ def lstm_3D(max_len,input_dim):
 
 def textcnn_2D(max_len,input_dim,output_dim):
     kernel_size = [2,3,4,5]
-    my_input = Input(shape=(max_len,), dtype='float64')
+    my_input = Input(shape=(max_len,))
     emb = Embedding(input_dim, output_dim, input_length=max_len)(my_input)
     emb = SpatialDropout1D(0.2)(emb)
 
@@ -43,3 +43,22 @@ def textcnn_2D(max_len,input_dim,output_dim):
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
+def textcnn_3D(max_len,input_dim):
+    kernel_size = [2,3,4,5]
+    my_input = Input(shape=(max_len,input_dim))
+    spa = SpatialDropout1D(0.2)(my_input)
+
+    net = []
+    for kernel in kernel_size:
+        con = Conv1D(32, kernel, activation='relu', padding="same")(spa)
+        con = MaxPool1D(2)(con)
+        net.append(con)
+    net = concatenate(net, axis=-1)
+    net = Flatten()(net)
+    net = Dropout(0.5)(net)
+    net = Dense(64, activation='relu')(net)
+    net = Dropout(0.5)(net)
+    net = Dense(1, activation='sigmoid')(net)
+    model = Model(inputs=my_input, outputs=net)
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
