da24ecb7a36bf1d29bcc0526d6ddd671ff6abd4b
404notfound <2811832487@qq.com>
2020-04-05 18:12:45 +0800

    [+] merge functions

--- feature_vec/model.py
+++ feature_vec/model.py
@@ -2,9 +2,14 @@ from keras.models import Sequential,Model
 from keras.layers import LSTM, Dense, Dropout,Input,InputLayer,SpatialDropout1D,Conv1D,MaxPool1D,Flatten,concatenate
 from keras.layers.embeddings import Embedding
 
-def lstm_2D(max_len,input_dim,output_dim):
+def lstm(max_len,input_dim,output_dim=None,weight_matrix=None,input_type='wordindex'):
     model = Sequential()
-    model.add(Embedding(input_dim, output_dim, input_length=max_len))
+    if input_type=='wordindex':
+        model.add(Embedding(input_dim, output_dim, input_length=max_len))
+    if input_type=='word2vec':
+        model.add(InputLayer(input_shape=(max_len,input_dim)))
+    if input_type=='word2vec_pretrain':
+        model.add(Embedding(input_dim, output_dim, input_length=max_len, weights=[weight_matrix], trainable=True))
     model.add(Dropout(0.5))
     model.add(LSTM(64, recurrent_dropout=0.5))
     model.add(Dropout(0.5))
@@ -12,21 +17,19 @@ def lstm_2D(max_len,input_dim,output_dim):
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-def lstm_3D(max_len,input_dim):
-    model = Sequential()
-    model.add(InputLayer(input_shape=(max_len,input_dim)))
-    model.add(Dropout(0.5))
-    model.add(LSTM(64, recurrent_dropout=0.5))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
-def textcnn_2D(max_len,input_dim,output_dim):
+def textcnn(max_len,input_dim,output_dim=None,weight_matrix=None,input_type='wordindex'):
     kernel_size = [2,3,4,5]
-    my_input = Input(shape=(max_len,))
-    emb = Embedding(input_dim, output_dim, input_length=max_len)(my_input)
-    emb = SpatialDropout1D(0.2)(emb)
+    if input_type=='wordindex':
+        my_input = Input(shape=(max_len,))
+        emb = Embedding(input_dim, output_dim, input_length=max_len)(my_input)
+        emb = SpatialDropout1D(0.2)(emb)
+    if input_type=='word2vec':
+        my_input = Input(shape=(max_len,input_dim))
+        emb = SpatialDropout1D(0.2)(my_input)
+    if input_type=='word2vec_pretrain':
+        my_input = Input(shape=(max_len,))
+        emb = Embedding(input_dim, output_dim, input_length=max_len, weights=[weight_matrix], trainable=True)(my_input)
+        emb = SpatialDropout1D(0.2)(emb)
 
     net = []
     for kernel in kernel_size:
@@ -43,22 +46,3 @@ def textcnn_2D(max_len,input_dim,output_dim):
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-def textcnn_3D(max_len,input_dim):
-    kernel_size = [2,3,4,5]
-    my_input = Input(shape=(max_len,input_dim))
-    spa = SpatialDropout1D(0.2)(my_input)
-
-    net = []
-    for kernel in kernel_size:
-        con = Conv1D(32, kernel, activation='relu', padding="same")(spa)
-        con = MaxPool1D(2)(con)
-        net.append(con)
-    net = concatenate(net, axis=-1)
-    net = Flatten()(net)
-    net = Dropout(0.5)(net)
-    net = Dense(64, activation='relu')(net)
-    net = Dropout(0.5)(net)
-    net = Dense(1, activation='sigmoid')(net)
-    model = Model(inputs=my_input, outputs=net)
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
