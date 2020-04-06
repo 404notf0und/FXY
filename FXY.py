321f25559967212271d6ad7c4cc3cedce8c2a066
404notfound <2811832487@qq.com>
2020-04-07 00:12:15 +0800

    [+] fixed issues

--- FXY.py
+++ FXY.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from feature_vec.model import lstm,textcnn
 
 # data load
-x1,y1=data_load('part2_url2.csv')
+x1,y1=data_load('part1_url.csv')
 x2,y2=data_load('part2_url2.csv')
 
 # feature engineering
@@ -16,7 +16,7 @@ fx2=nlp.transform(x2) # test data to vec
 
 # model training
 train_x, valid_x, train_y, valid_y = train_test_split( fx1, fy1, random_state=2019,test_size = 0.3) 
-model=textcnn(input_type='word2vec',max_len=nlp.max_length,input_dim=nlp.input_dim,output_dim=16,class_num=3)
+model=textcnn(input_type='word2vec',max_len=nlp.max_length,input_dim=nlp.input_dim,output_dim=16,class_num=1)
 model.fit(train_x, train_y, validation_data=(valid_x,valid_y), epochs=1, batch_size=128)
 
 # model testing
