8e15347ebdc890d19d6201f20f54f6dc54869ca4
404notfound <2811832487@qq.com>
2020-04-08 16:21:51 +0800

    [+] fixed bug caused by function 'reset_index'

--- feature_vec/lib.py
+++ feature_vec/lib.py
@@ -12,7 +12,7 @@ def data_load(file=None):
     if file:
         file=os.path.join(datadir,file)
         samples=pd.read_csv(file)
-        samples=samples.sample(frac=1)
+        samples=samples.sample(frac=1).reset_index()
         x=samples['payload'].astype('str')
         x=x.apply(decode)
         if 'label' in samples.columns:
@@ -39,5 +39,37 @@ def model_metrics(true,false):
     print(classification_report(true,pre))
     pass
 
-
+# data_clean
+# webshell samples traversal
+def dir_file(dir_path,label):
+    results=[]
+    labels=[]
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        for name in filenames:
+            #print(name)
+            if '.php' in name:
+                path=(dirpath+'/'+name)
+                labels.append(label)
+                result=[]
+                with open(path,encoding='utf8',errors='ignore') as f:
+                    file=f.read().replace('\n',' ').split(' ')
+                    file=[l.strip() for l in file if l]
+                files=' '.join(file)
+                results.append(files)
+    
+    data=pd.DataFrame(results)
+    labels=pd.DataFrame(labels)
+    result=pd.concat([data,labels],axis=1,ignore_index=True)
+    result.columns=['payload','label']
+    return result
+# white=dir_file('C:\\Users\\Administrator\\Desktop\\typecho-master\\typecho-master',0)
+# print(white.shape)
+# white2=dir_file('C:\\Users\\Administrator\\Desktop\\WordPress-master\\WordPress-master',0)
+# print(white2.shape)
+# black=dir_file('C:\\Users\\Administrator\\Desktop\\webshell-sample-master\\webshell-sample-master',1)
+# print(black.shape)
+# result=pd.concat([white,white2,black],ignore_index=True)
+# result.to_csv('part5A_webshell.csv',encoding='utf_8_sig',index=None)
+        
+        
     
