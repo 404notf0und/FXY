e9b4b1c7c2245c4224a687d75880dfa309b83b1f
404notfound <2811832487@qq.com>
2020-04-09 19:34:38 +0800

    [+] data preprocessing

--- feature_vec/lib.py
+++ feature_vec/lib.py
@@ -12,7 +12,7 @@ def data_load(file=None):
     if file:
         file=os.path.join(datadir,file)
         samples=pd.read_csv(file)
-        samples=samples.sample(frac=1,random_state=2020).reset_index()
+        samples=samples.sample(frac=1,random_state=2020).reset_index(drop=True)
         x=samples['payload'].astype('str')
         x=x.apply(decode)
         if 'label' in samples.columns:
@@ -62,7 +62,7 @@ def dir_file(dir_path,label):
     result=pd.concat([data,labels],axis=1,ignore_index=True)
     result.columns=['payload','label']
     return result
-# white=dir_file('C:\\Users\\Administrator\\Desktop\\typecho-master\\typecho-master',0)
+#white=dir_file('C:\\Users\\Administrator\\Desktop\\typecho-master\\typecho-master',0)
 # print(white.shape)
 # white2=dir_file('C:\\Users\\Administrator\\Desktop\\WordPress-master\\WordPress-master',0)
 # print(white2.shape)
@@ -70,6 +70,31 @@ def dir_file(dir_path,label):
 # print(black.shape)
 # result=pd.concat([white,white2,black],ignore_index=True)
 # result.to_csv('part5A_webshell.csv',encoding='utf_8_sig',index=None)
-        
-        
+
+def dir_file_mix(dir_path):
+    results=[]
+    labels=[]
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        for name in filenames:
+            #print(name)
+            path=(dirpath+'/'+name)
+            if 'spm' in name:
+                labels.append(1)
+            else:
+                labels.append(0)
+            result=[]
+            with open(path,encoding='utf8',errors='ignore') as f:
+                file=f.read().replace('\n',' ').split(' ')
+                file=[l.strip() for l in file if l]
+            files=' '.join(file)
+            results.append(files)
     
+    data=pd.DataFrame(results)
+    labels=pd.DataFrame(labels)
+    result=pd.concat([data,labels],axis=1,ignore_index=True)
+    result.columns=['payload','label']
+    return result
+#mix_data=dir_file_mix('D:\\Documents\\GitHub\\LJDM\\ling-spam\\train-mails')      
+#mix_data.to_csv('part6A_spamail.csv',encoding='utf_8_sig',index=None)        
+#mix_data2=dir_file_mix('D:\\Documents\\GitHub\\LJDM\\ling-spam\\test-mails')      
+#mix_data2.to_csv('part6B_spamail.csv',encoding='utf_8_sig',index=None)   
